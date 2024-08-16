7ad2446ba9bee4986708e6283c0521343426999c
Doğu İlmak <doguilmak@gmail.com>
2024-08-16 19:19:08 +0300

    Update inference_vision.py
    Version 1.1 update!

--- inference_vision.py
+++ inference_vision.py
@@ -5,20 +5,64 @@ import numpy as np
 import pandas as pd
 
 class InferenceVision:
+    
+    VERSION = "1.1"
 
-    VERSION = "1.0"
+    def __init__(self, tif_path, model_path):
+        """
+        Initialize an InferenceVision instance.
 
-    def __init__(self, tif_path, model_path, image_width, image_height):
+        Parameters
+        ----------
+        tif_path : str
+            The file path to the TIFF image to be processed.
+        model_path : str
+            The file path to the YOLO model weights.
+
+        Returns
+        -------
+        None
+            This method initializes the object and does not return any value.
+
+        Example
+        -------
+        Create an instance of InferenceVision
+        >>> inference = InferenceVision(
+                tif_path="path/to/image.tif",
+                model_path="path/to/model.pt"
+            )
+        """
         self.tif_path = tif_path
         self.model_path = model_path
-        self.image_width = image_width
-        self.image_height = image_height
         self.extracted_polygons_lonlat = []
         self.results = []
         self.center_coords = []
         self.extracted_polygons = []
 
+        with rasterio.open(self.tif_path) as dataset:
+            self.image_width = dataset.width
+            self.image_height = dataset.height
+
     def normalize_center_points(self, center_points):
+        """
+        Normalize center points relative to the image dimensions.
+
+        Parameters
+        ----------
+        center_points : list of tuples
+            A list of tuples where each tuple contains (x_center, y_center) coordinates.
+
+        Returns
+        -------
+        list of lists
+            A list of normalized coordinates where each coordinate is [normalized_x, normalized_y].
+
+        Example
+        -------
+        Given center points [(50, 100), (150, 200)]
+        >>> normalized_points = inference.normalize_center_points([(50, 100), (150, 200)])
+        normalized_points will be [[0.048828125, 0.12962962962962962], [0.146484375, 0.25925925925925924]]
+        """
         normalized_points = []
         for x_center, y_center in center_points:
             normalized_x = x_center / self.image_width
@@ -27,6 +71,25 @@ class InferenceVision:
         return normalized_points
 
     def calculate_bbox_center(self, coordinates):
+        """
+        Calculate the center point of a bounding box.
+
+        Parameters
+        ----------
+        coordinates : list
+            A list of four values representing the bounding box coordinates [x_min, y_min, x_max, y_max].
+
+        Returns
+        -------
+        tuple
+            A tuple (x_center, y_center) representing the center of the bounding box.
+
+        Example
+        -------
+        Given bounding box coordinates [10, 20, 30, 40]
+        >>> center = inference.calculate_bbox_center([10, 20, 30, 40])
+        Center will be (20.0, 30.0)
+        """
         x_min, y_min, x_max, y_max = coordinates
         width = x_max - x_min
         height = y_max - y_min
@@ -35,8 +98,33 @@ class InferenceVision:
         return x_center, y_center
 
     def process_image(self, build_csv=False, csv_filename=None):
+        """
+        Process the image using the YOLO model and optionally save the results to a CSV file.
+
+        Parameters
+        ----------
+        build_csv : bool, optional
+            Whether to build and save the CSV file with results. If True, the CSV file
+            will be saved with the name provided in `csv_filename`. Default is False.
+        csv_filename : str, optional
+            The name of the CSV file to save results, if `build_csv` is True.
+
+        Returns
+        -------
+        None
+            If `build_csv` is True, saves results to the specified CSV file. If False,
+            prints results to the console.
+
+        Example
+        -------
+        Process the image and save results to a CSV file
+        >>> inference.process_image(build_csv=True, csv_filename="output.csv")
+
+        Process the image and print results to the console
+        >>> inference.process_image(build_csv=False)
+        """
         model = YOLO(self.model_path)
-        # results = model.predict(self.tif_path, save=True)
+
         results = model.predict(self.tif_path)
 
         with rasterio.open(self.tif_path) as dataset:
@@ -48,20 +136,14 @@ class InferenceVision:
             self.extracted_polygons[0] = self.extracted_polygons[0][:-1]
             self.extracted_polygons_lonlat = self.extracted_polygons[0]
 
-            for result in results:
-                boxes = result.boxes
-                masks = result.masks
-                keypoints = result.keypoints
-                probs = result.probs
-
             result = results[0]
-            detected_objects= len(result.boxes)
+            detected_objects = len(result.boxes)
 
             for i in range(detected_objects):
-              box = result.boxes[i]
-              coordinates = box.xyxy[0].tolist()
-              x_center, y_center = self.calculate_bbox_center(coordinates)
-              self.center_coords.append([x_center, y_center])
+                box = result.boxes[i]
+                coordinates = box.xyxy[0].tolist()
+                x_center, y_center = self.calculate_bbox_center(coordinates)
+                self.center_coords.append([x_center, y_center])
 
             normalized_points = self.normalize_center_points(self.center_coords)
 
@@ -71,7 +153,6 @@ class InferenceVision:
             lon_bottom_right = self.extracted_polygons[0][2][1]
 
             normalized_coords = np.array(normalized_points)
-
             geographic_coords = np.empty_like(normalized_coords)
             for i in range(normalized_coords.shape[0]):
                 y_norm, x_norm = normalized_coords[i]
@@ -84,41 +165,38 @@ class InferenceVision:
             ])
 
             if build_csv:
-              data_list = []
+                data_list = []
+                for i in range(detected_objects):
+                    box = result.boxes[i]
+                    class_id = result.names[box.cls[0].item()]
+                    coordinates = box.xyxy[0].tolist()
+                    x_center, y_center = self.calculate_bbox_center(coordinates)
+                    normalized_point = normalized_points[i]
 
-              for i in range(detected_objects):
-                  box = result.boxes[i]
-                  class_id = result.names[box.cls[0].item()]
-                  coordinates = box.xyxy[0].tolist()
-                  x_center, y_center = self.calculate_bbox_center(coordinates)
-                  normalized_point = normalized_points[i]
+                    data_list.append({
+                        "Image": self.tif_path,
+                        "Point": i,
+                        "Latitude": formatted_geographic_coords[i][1],
+                        "Longitude": formatted_geographic_coords[i][0],
+                        "Object Type": class_id,
+                        "Coordinates": box.xyxy[0].tolist(),
+                        "Probability": box.conf[0].item(),
+                        "Bounding Box Center": [x_center, y_center],
+                        "Normalized Bounding Box Center": normalized_point
+                    })
 
-                  data_list.append({
-                      "Image": self.tif_path,
-                      "Point": i,
-                      "Latitude": formatted_geographic_coords[i][1],
-                      "Longitude": formatted_geographic_coords[i][0],
-                      "Object Type": class_id,
-                      "Coordinates": box.xyxy[0].tolist(),
-                      "Probability": box.conf[0].item(),
-                      "Bounding Box Center": [x_center, y_center],
-                      "Normalized Bounding Box Center": normalized_point
-                  })
-
-              df = pd.DataFrame(data_list)
-              df.to_csv(csv_filename, index=False)
-              print(f"\n\nDataFrame saved as {csv_filename}")
+                df = pd.DataFrame(data_list)
+                df.to_csv(csv_filename, index=False)
+                print(f"\n\nDataFrame saved as {csv_filename}")
 
             else:
-              for i in range(detected_objects):
-                box = result.boxes[i]
-                print("-"*30)
-                class_id = result.names[box.cls[0].item()]
-                print(f"Point {i}")
-                print(f"Latitude: {formatted_geographic_coords[i][1]} - Longitude: {formatted_geographic_coords[i][0]} ({formatted_geographic_coords[i][1]}, {formatted_geographic_coords[i][0]})")
-                print("Object type:", class_id)
-                print("Object type:", box.cls[0].item())
-                print("Coordinates:", box.xyxy[0].tolist())
-                print("Probability:", box.conf[0].item())
-                print("Bounding Box Center:", [x_center, y_center])
-                print("Normalized Bounding Box Center:", [normalized_points[i][0], normalized_points[i][1]])
+                for i in range(detected_objects):
+                    box = result.boxes[i]
+                    print(f"\nPoint {i} ", "-" * 40)
+                    class_id = result.names[box.cls[0].item()]
+                    print(f"Latitude: {formatted_geographic_coords[i][1]} - Longitude: {formatted_geographic_coords[i][0]}")
+                    print("Object type:", class_id)
+                    print("Coordinates:", box.xyxy[0].tolist())
+                    print("Probability:", box.conf[0].item())
+                    print("Bounding Box Center:", [x_center, y_center])
+                    print("Normalized Bounding Box Center:", [normalized_points[i][0], normalized_points[i][1]])
